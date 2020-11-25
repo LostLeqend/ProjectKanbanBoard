291b8f66465b98c58782bf731f379e1b901da661
Raphael Härtel <raphael.haertel@bluewin.ch>
2020-11-25 13:56:03 +0100

    Implement Drag and Drop + Refactoring

--- kanban-frontend/app.js
+++ kanban-frontend/app.js
@@ -12,7 +12,7 @@ document.getElementById("btn-close").addEventListener('click', () => {
     addTaskPopup.style.display = "none";
 });
 
-document.getElementById("btn-add").addEventListener('click', () =>{
+document.getElementById("btn-add").addEventListener('click', () => {
     let taskname = document.getElementById("txt-taskname").value;
     console.log();
     addTaskPopup.style.display = "none";
@@ -24,3 +24,24 @@ window.onclick = function (event) {
         addTaskPopup.style.display = "none";
     }
 }
+
+///Drag and Drop
+var startingDropzone;
+
+document.addEventListener("dragstart", function (event) {
+    startingDropzone = event.target;
+}, false);
+
+document.addEventListener("dragover", function (event) {
+    event.preventDefault();
+}, false);
+
+document.addEventListener("drop", function (event) {
+    event.preventDefault();
+
+    if (event.target.className.includes("dropzone")) {
+        event.target.style.background = "";
+        startingDropzone.parentNode.removeChild(startingDropzone);
+        event.target.insertBefore(startingDropzone, event.target.firstChild);
+    }
+}, false);
