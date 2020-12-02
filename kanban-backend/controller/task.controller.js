cf18bdcdd4f377466a89c3aaff35582211856b6f
the-philip-baumann <philip.baumann@hispeed.ch>
2020-12-02 10:59:42 +0100

    Implement backend validation and response.status

--- kanban-backend/controller/task.controller.js
+++ kanban-backend/controller/task.controller.js
@@ -1,4 +1,4 @@
-import {Application, Router, send} from 'https://deno.land/x/oak@v6.3.1/mod.ts';
+import {Application, Router} from 'https://deno.land/x/oak@v6.3.1/mod.ts';
 import {oakCors} from "https://deno.land/x/cors/mod.ts";
 import {Task} from "../classes/Task.js";
 import {State} from "../classes/State.js";
@@ -23,16 +23,21 @@ router.get('/columns', (context) => {
 router.post('/add', async (context) => {
     if (context.request.hasBody) {
         const body = JSON.parse(await context.request.body().value);
-        const task = new Task();
-        task.title = body.title;
-        task.state = body.state;
-        task.id = id;
-        tasks.push(task);
-        id++;
-        context.response.state = 200;
-        context.response.body = 'Task with id: ' + id + 'has been created';
+        if (Object.values(State).includes(body.state) && body.title.trim() !== "") {
+            const task = new Task();
+            task.title = body.title;
+            task.state = body.state;
+            task.id = id;
+            tasks.push(task);
+            id++;
+            context.response.status = 200;
+            context.response.body = 'Task with id: ' + id + 'has been created';
+        } else {
+            context.response.status = 400;
+            context.response.body = 'Violation of parameter value';
+        }
     } else {
-        context.response.state = 400;
+        context.response.status = 400;
         context.response.body = 'Request has no body';
     }
 });
@@ -40,14 +45,14 @@ router.post('/add', async (context) => {
 router.delete('/delete/:id', async (context) => {
     if (context.params && context.params.id) {
         const task = tasks.find(function (task) {
-            return task.id == context.params.id;
+            return task.id === context.params.id;
         });
         const index = tasks.indexOf(task);
         tasks.splice(index, 1);
-        context.response.state = 200;
+        context.response.status = 200;
         context.response.body = 'Item with id ' + context.params.id + ' was deleted';
     } else {
-        context.response.state = 400;
+        context.response.status = 400;
         context.response.body = "No parameter found"
     }
 });
@@ -55,14 +60,19 @@ router.delete('/delete/:id', async (context) => {
 router.post('/move', async (context) => {
     if (context.request.hasBody) {
         const body = JSON.parse(await context.request.body().value);
-        const task = tasks.find(function (task) {
-            return task.id == body.id;
-        });
-        task.state = body.state;
-        context.response.state = 200;
-        context.response.body = 'Task with id:' + body.id + ' has been updated with state:' + body.state;
+        if (Object.values(State).includes(body.state)) {
+            const task = tasks.find(function (task) {
+                return task.id === body.id;
+            });
+            task.state = body.state;
+            context.response.status = 200;
+            context.response.body = 'Task with id:' + body.id + ' has been updated with state:' + body.state;
+        } else {
+            context.response.status = 400;
+            context.response.body = 'Violation of parameter value';
+        }
     } else {
-        context.response.state = 400;
+        context.response.status = 400;
         context.response.body = 'Request has no body';
     }
 });
